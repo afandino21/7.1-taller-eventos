9cc42bbf1bdfda7e200c1dc4a5f41bfc764f9332
¨afandino21¨ <al_fl95@hotmail.com>
2023-08-14 15:29:51 -0300

    solucion al click

--- eventos.js
+++ eventos.js
@@ -3,3 +3,9 @@ const element = document.getElementById("divV");
 element.addEventListener("click", function () {
     alert("Hola! Soy el Div");
 });
+
+const botonElement = document.getElementById("boton");
+botonElement.addEventListener('click', function (event) {
+    event.stopPropagation();
+    alert("Hola!");
+});
